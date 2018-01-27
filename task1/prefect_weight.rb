90337ab9b5649c1c4cfdbfee7b116b41facd58c6
alexfilus <alexfilus@gmail.com>
2018-01-27 11:20:57 -0800

    Задание к уроку "Основы Ruby. Часть 1" Правки

--- task1/prefect_weight.rb
+++ task1/prefect_weight.rb
@@ -9,7 +9,6 @@ weight = gets.to_i
 
 perfect_weight = height - 110
 
-
 if weight < perfect_weight
   puts "Ваш вес уже оптимальный"
 else
--- task1/right_triagle.rb
+++ task1/right_triagle.rb
@@ -1,14 +1,14 @@
 puts "Введите стороны A, B, C через запятую: "
 
-a, b, c  = gets.split(',').map{|item| item.to_f}.sort.reverse
+a, b, c = gets.split(',').map {|item| item.to_f}.sort.reverse
 
 if a ** 2 == b ** 2 + c ** 2
   puts "Треугольник прямоугольный"
 end
 
-if b == c || a==c || a==b
+if b == c || a == c || a == b
   puts "Треугольник равнобедренный"
-  if b == c && a==c && a==b
+  if b == c && a == c && a == b
     puts "Треугольник равносторонний"
   end
 end
